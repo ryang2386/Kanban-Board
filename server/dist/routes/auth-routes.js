5fc55de8d6afd9762d5df0841b4ff9dc8ececa50
Ryan Gayle <gayler02@nyu.edu>
2025-04-12 18:13:07 -0400

    entered code in auth-routes.ts

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    // TODO: If the user exists and the password is correct, return a JWT token
+    const { username, password } = req.body;
+    const user = await User.findOne({
+        where: { username },
+    });
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+};
+const router = Router();
+// POST /login - Login a user
+router.post('/login', login);
+export default router;
