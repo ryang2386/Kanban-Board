03cf09b61f3d4bfc158074cc013aca97aeb81a8f
Ryan Gayle <gayler02@nyu.edu>
2025-04-12 17:10:40 -0400

    entered login logic in authAPI.tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,26 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+  }
+
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error('Error logging in:', error);
+    return Promise.reject('User information is not found.');
+  }
 }
 
 
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,4 @@
+-- Active: 1743119442391@@127.0.0.1@5432@kanban_db
 -- DROP DATABASE
 DROP DATABASE IF EXISTS kanban_db;
 
